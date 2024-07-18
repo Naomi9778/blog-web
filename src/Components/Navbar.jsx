61d6b4c7ae1b9c4c57da6ef3bf69017c6a63388d
Naomi9778 <namuun.namuun9778@gmail.com>
2024-07-18 18:17:03 +0800

    blogwebstart

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Logo } from "./Icons/Logo";
+
+export const Navbar = () => {
+
+    return (
+        <div>
+            <Logo/>
+            <button></button>
+        </div>
+    )
+}
